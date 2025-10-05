f5d2d87144f2a4d6097f1b9273f93b214936cd73
Sung-Yu Lin <sungyulin@uvic.ca>
2025-10-05 15:12:19 -0700

    meteor appear

--- src/App.tsx
+++ src/App.tsx
@@ -26,6 +26,7 @@ export default function App() {
             <Starfield />
             <Globe />
             <Effects />
+            {mode !== 'story' && <Asteroid />}
             <ImpactOverlays />
           </Suspense>
         </Canvas>
